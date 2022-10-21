5644617e108e4ceeb60becf868f1ba991e433bd5
carl kevin gbeti <carlkof@hotmail.com>
2022-10-21 09:18:40 -0400

    debut gui

--- Code/App/src/Client/Module/landing/vue_landing.py
+++ Code/App/src/Client/Module/landing/vue_landing.py
@@ -1,10 +1,13 @@
-from ast import Pass
-import tkinter as tk
-from tkinter import ttk
+from tkinter import *
+from tkinter.ttk import *
 
 class Vue_landing():
     def __init__(self):
         super().__init__()
+        self.title = "Landing page"
+        self.width = 1280
+        self.height = 720
+        self.master = None
         self.controleur = None
         self.remplir_vue()
 
@@ -12,5 +15,9 @@ class Vue_landing():
         self.controleur = controleur
 
     def remplir_vue(self):
-        ttk.Frame()
+        self.master = Tk()
+        label = Label(self.master, text="test")
+        label.pack()
+        
+        
         
